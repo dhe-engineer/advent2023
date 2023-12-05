2f3092c6428f79fe272dae4cb737b858b5460135
dhe-engineer <>
2023-12-05 09:45:45 +0000

    Fixed day 4 part B code

--- day4/day4b.py
+++ day4/day4b.py
@@ -5,7 +5,7 @@ def parse_file(filename):
     num_of_chars = len(data_file[0])
 
     new_scratch_card = []
-    for numebrs in range(num_of_lines):
+    for numbers in range(num_of_lines):
         new_scratch_card.append(int(1))
 
     data_in = []
@@ -25,19 +25,19 @@ def parse_file(filename):
                 if winning_item == card_item:
                     matches += 1
 
-            for winning_range in range(0, number_of_card_copies):
-                for increment_number in range(line_number+1, matches):
-                    if increment_number > num_of_lines:
-                        break
-                    current_number = int(new_scratch_card[increment_number])
-                    new_scratch_card[increment_number] = current_number + 1
-                    new_scratch_card[increment_number] = current_number + 1
+        for winning_range in range(0, number_of_card_copies):
+            for increment_number in range(line_number+1, line_number+matches+1):
+                if increment_number > num_of_lines:
+                    break
+                current_number = int(new_scratch_card[increment_number])
+                new_scratch_card[increment_number] = current_number + 1
 
-            total_points += matches
+        total_points += matches
 
     print(f"Total points: {total_points}, Total cards: {
           sum(new_scratch_card)}")
 
 
 if __name__ == "__main__":
-    parse_file(filename="day4/example.txt")
+    # parse_file(filename="day4/example.txt")
+    parse_file(filename="day4/puzzle.txt")
